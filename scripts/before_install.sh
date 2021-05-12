7eebf1db663a740f4621aa63c118b87cd6887fe9
saksham jhawar <sj.sakshamjhawar@gmail.com>
2021-05-12 00:56:26 -0700

    added script to unzip build file on EC2

---
+++ scripts/before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ec2-user/server
+sudo yum install unzip
+unzip TestPipelineBuild.zip
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+yum -y install nodejs npm
